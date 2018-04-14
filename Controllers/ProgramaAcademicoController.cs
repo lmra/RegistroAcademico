d81dfed3899c1d225fc976113bb78e5a9b370092
laura.rodrigueza@udea.edu.co <dg553152>
2018-04-14 18:21:53 -0500

    Initial commit

---
+++ Controllers/ProgramaAcademicoController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RegistroAcademico.Controllers
+{
+    public class ProgramaAcademicoController : Controller
+    {
+        // GET: ProgramaAcademico
+        public ActionResult index()
+        {
+            return View();
+        }
+
+        public ActionResult Estudiante()
+        {
+            return View();
+        }
+        public ActionResult Docente()
+        {
+            return View();
+        }
+
+        public ActionResult Grupos()
+        {
+            return View();
+        }
+
+        public ActionResult Inscripcion()
+        {
+            return View();
+        }
+
+        public ActionResult Calificacion()
+        {
+            return View();
+        }
+
+        public ActionResult Cursos()
+        {
+            return View();
+        }
+
+        public ActionResult Universidad()
+        {
+            return View();
+        }
+
+    }
+}
